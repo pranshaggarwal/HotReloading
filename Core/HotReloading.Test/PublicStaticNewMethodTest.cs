6b9f6c60c87d00db04bdf11f60b4aff4942e99b0
Pranshu Aggarwal <pranshu@xenolt.com>
2019-04-14 22:31:51 +0530

    Added some test cases

---
+++ Core/HotReloading.Test/PublicStaticNewMethodTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace HotReloading.Test
+{
+    [TestFixture]
+    public class PublicStaticNewMethodTest
+    {
+        public void AddMethodAndCalledFromSameClass()
+        {
+
+        }
+
+        public void AddMethodAndCalledFromAnotherClass()
+        {
+
+        }
+    }
+}
---
+++ Core/HotReloading.Test/PublicStaticUpdateMethodTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HotReloading.Test
+{
+    [TestFixture]
+    public class PublicStaticUpdateMethodTest
+    {
+        public void MethodWithNoParameterNoReturnValue()
+        {
+
+        }
+
+        public void MethodWithNoParameterButReturnValue()
+        {
+
+        }
+
+        public void MethodWithNoReturnValueButPattern()
+        {
+
+        }
+
+        public void MethodWithBothParameterAndReturnValue()
+        {
+
+        }
+
+        public void TaskAsycnMethodWithNoReturnValue()
+        {
+
+        }
+
+        public void TaskAsyncMethodWithReturnValue()
+        {
+
+        }
+
+        public void MethodOverload()
+        {
+
+        }
+    }
+}
