e3eed4f6efa59e9f887ef6cee3ce6971c414e2c6
Pranshu Aggarwal <pranshu@xenolt.com>
2019-04-29 07:33:38 +0530

    Adding hotreloading commands for VS Mac

---
+++ IDE/VisualStudio.Mac/Commands/AllowOverrideCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+
+namespace VisualStudio.Mac.Commands
+{
+    public class AllowOverrideCommands : CommandHandler
+    {
+        private CommandInfo info;
+        protected override void Run()
+        {
+            Settings.AllowOverride = !info.Checked;
+        }
+
+        protected override void Update(CommandInfo info)
+        {
+            this.info = info;
+            this.info.Enabled = Settings.Enabled;
+            this.info.Checked = Settings.AllowOverride;
+        }
+    }
+}
---
+++ IDE/VisualStudio.Mac/Commands/EnableCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+
+namespace VisualStudio.Mac.Commands
+{
+    public class EnableCommands : CommandHandler
+    {
+        private CommandInfo info;
+
+        protected override void Run()
+        {
+            Settings.Enabled = !info.Checked;
+        }
+
+        protected override void Update(CommandInfo info)
+        {
+            this.info = info;
+            info.Enabled = true;
+            info.Checked = Settings.Enabled;
+        }
+    }
+}
